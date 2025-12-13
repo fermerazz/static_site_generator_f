b152c54f94500c8888498be8dbc3579ee3e49faf
fermerazz <fermerazz12@gmail.com>
2025-12-13 00:00:52 -0600

    modified main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
-from htmlnode import ParentNode
-from inline_markdown import text_to_textnodes
-from textnode import text_node_to_html_node, TextNode, TextType
+
 from copystatic import copy_static
 
 
         
 def main():
-    node = TextNode("Hello darkness my old friend", TextType.TEXT, "https://www.boot.dev")
-    print(node)
+    copy_static("static", "public")
 
-main()
+if __name__ == "__main__":
+    main()
