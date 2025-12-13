01d833423b99ef264a87114222837c17be1903e7
fermerazz <fermerazz12@gmail.com>
2025-12-13 16:35:46 -0600

    Added more pages and content

--- src/main.py
+++ src/main.py
@@ -5,5 +5,6 @@ from gencontent import generate_page
 def main():
     copy_static("static", "public")
     generate_page("content/index.md", "template.html", "public/index.html")
+
 if __name__ == "__main__":
     main()
